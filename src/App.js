3a5e3b039dfd91bd67179b4b0404fbc6d4d7ba9d
Aggie Koko <agata.kosior57@gmail.com>
2018-07-03 17:41:56 +0200

    bookshelf props and styling added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
 import Bookshelf from './Bookshelf.js';
 import './App.css';
+import * as BooksAPI from './BooksAPI.js'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      all : [],
+      currently : [1, 2, 3],
+      toRead : [],
+      read : []
+    }
+  }
+
+  componentDidMount() {
+      BooksAPI.getAll().then(books => {
+      this.setState({ all : books }) })
+    }
+
+
   render() {
     return (
+      <div>
+      <nav className="navbar">
+        <h1>My Reads</h1>
+      </nav>
       <div className="main">
-        <Bookshelf name="Currently reading" />
-        <Bookshelf name="To read" />
-        <Bookshelf name="Read" />
+        <Bookshelf name="Currently reading" books={this.state.currently}/>
+        <Bookshelf name="To read" books={this.state.toRead}/>
+        <Bookshelf name="Read" books={this.state.read}/>
       </div>
+      </div>
+
     );
   }
 }
--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react'
 import './Bookshelf.css'
+import Book from './Book.js'
 
 
 
 class Bookshelf extends Component {
+
+
   render() {
+    const { books } = this.props
     return (
     <div className = "bookshelf">
       <h2 className = "bookshelf-name"> {this.props.name} </h2>
+
         <ul className="booklist">
-          <li className="book"></li>
+{books.length == 0 && <p>Your bookshelf is currently empty.</p>}
+        {books.map((book, id) => (
+        <Book key={id}/>
+))}
+
+
+
         </ul>
     </div>
     )
